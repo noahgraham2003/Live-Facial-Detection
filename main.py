57ffc4e608a9a66a2ca92f2b52bc6a3c10253f03
Noah Graham <70675251+noahgraham2003@users.noreply.github.com>
2023-08-01 14:50:36 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import threading
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+counter = 0
+face_match = False
+reference_img = cv2.imread("reference.jpg")
+
+def check_face(frame):
+    global face_match
+    try:
+        face_match = False
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            face_match = True
+    except ValueError:
+        face_match = False
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
+
+        # Detect and draw bounding box around the face
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        for (x, y, w, h) in faces:
+            if face_match:
+                color = (0, 255, 0)  # Green color for matching face
+            else:
+                color = (0, 0, 255)  # Red color for non-matching face
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+        if face_match:
+            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow("video", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
